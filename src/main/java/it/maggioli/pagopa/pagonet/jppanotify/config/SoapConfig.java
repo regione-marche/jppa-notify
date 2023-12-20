4c3d05fd5e4e456cedaddd1ce75343d23f316d42
Dario Licci <dario.licci@maggioli.it>
2023-12-20 10:16:25 +0100

    fix pom dependencies

--- src/main/java/it/maggioli/pagopa/pagonet/jppanotify/config/SoapConfig.java
+++ src/main/java/it/maggioli/pagopa/pagonet/jppanotify/config/SoapConfig.java
@@ -14,10 +14,12 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 import it.maggioli.pagopa.pagonet.jppanotify.api.soap.NotifyEndpoint;
 import jakarta.servlet.Servlet;
 
+@ImportResource({"classpath:META-INF/cxf/cxf.xml"})
 @Configuration
 public class SoapConfig
 {
